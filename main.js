707d4121ca197cd2c63b90f01bf30df45eae4f58
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2023-09-29 23:27:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-// Pom ---------------------------------------------
-function tocaSomPom () {
-    document.querySelector ('#som_tecla_pom').play ();
+function tocaSom (idElementoAudio) {
+    document.querySelector (idElementoAudio).play();
 }
 
-document.querySelectorAll('.tecla');
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
